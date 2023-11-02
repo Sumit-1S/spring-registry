939aad1c66628520096661fb60d009a1bd92a07a
= <=>
2023-11-02 07:09:16 -0700

    Initial Commit

---
+++ src/test/java/com/example/springmicroserver/SpringMicroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmicroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
