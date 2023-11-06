5a7e0243aa870a6811b4f372faf7f43a402d6f8f
= <=>
2023-11-06 09:50:22 -0800

    Patch 1 Sumit

--- src/main/java/com/example/springmicroserver/RegistryMicroService.java
+++ src/main/java/com/example/springmicroserver/RegistryMicroService.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringMicroServerApplication {
+public class RegistryMicroService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMicroServerApplication.class, args);
+		SpringApplication.run(RegistryMicroService.class, args);
 	}
 
 }
